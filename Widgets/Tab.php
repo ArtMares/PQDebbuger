f7d24fb8d1c798ab6009507995af98c4bdc93e43
Dmitriy Dergachev <dergachevdv90@gmail.com>
2020-01-10 16:43:22 +0300

    Добавил автозагрузчик PHP классов

---
+++ Widgets/Tab.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author:             Dmitriy Dergachev (ArtMares)
+ * @date:               10.01.2020
+ */
+
+namespace Widgets;
+
+
+class Tab
+{
+
+}
--- scripts/main.php
+++ scripts/main.php
@@ -33,11 +33,29 @@
 //require_once 'Json.php';
 //require_once 'ObjectWidget.php';
 
-require_once 'qrc://scripts/Tab.php';
-require_once 'qrc://scripts/ListWidget.php';
-require_once 'qrc://scripts/ObjectWidget.php';
-require_once 'qrc://scripts/Json.php';
-require_once 'qrc://scripts/Icon.php';
+spl_autoload_register(function($className) {
+    $file = str_replace('\\', '/', $className).'.php';
+    $qf = new QFile();
+    $qf->setFileName(':/scripts/'.$file);
+    if($qf->exists()) {
+        require_once('qrc://scripts/'.$file);
+        return true;
+    } else {
+        $qf->setFileName($file);
+        if($qf->exists()) {
+            require_once($file);
+            return true;
+        }
+    }
+    return false;
+});
+
+
+//require_once 'qrc://scripts/Tab.php';
+//require_once 'qrc://scripts/ListWidget.php';
+//require_once 'qrc://scripts/ObjectWidget.php';
+//require_once 'qrc://scripts/Json.php';
+//require_once 'qrc://scripts/Icon.php';
 
 $app = new QApplication($argc, $argv);
 
@@ -47,5 +65,7 @@ $app = new QApplication($argc, $argv);
 require_once 'qrc://scripts/PHPQt5Debugger.php';
 $window = new PHPQt5Debugger();
 $window->showMaximized();
+
+$t = new \Widgets\Tab();
 
 return $app->exec();
